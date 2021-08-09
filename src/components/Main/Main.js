e57f33a31b6f63fdeb4aea7ea271ab4521173e36
Kevin Martin <kevin@Mac-mini.local>
2021-08-09 14:22:15 +0200

    fix mobile

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,6 +7,21 @@ import "./Main.scss";
 
 const slideProperties = {
   autoplay: false,
+  prevArrow: (
+    <div style={{ width: "30px", marginRight: "-30px" }}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff">
+        <path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z" />
+      </svg>
+    </div>
+  ),
+  nextArrow: (
+    <div style={{ width: "30px", marginLeft: "-30px" }}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff">
+        <path d="M512 256L270 42.6v138.2H0v150.6h270v138z" />
+      </svg>
+    </div>
+  ),
+  transitionDuration: 500,
 };
 
 const Main = () => {
