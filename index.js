722c24b5a1b8847fe35079d901993beedf21bac3
Kevin <coldfuzion.kvm@gmail.com>
2022-06-04 21:24:03 +0200

    fix vh

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import App from "./src/components/Main/Main";
 
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// All resize
+window.addEventListener('resize', () => {
+  const vh2 = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh2}px`);
+});
+
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
